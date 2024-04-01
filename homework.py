ba2f2461af558fb684881f0fd7f9ede4dcbcf77d
nisszhra <149693649+nisszhra@users.noreply.github.com>
2024-04-01 22:37:35 +0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -5,10 +5,13 @@ nilai = int(input("Masukan Nilai : "))
 print(40*'=')
 print("Mahasiswa dengan nama", nama_mahasiswa)
 if (nilai >= 81) and (nilai <= 100):
-    print("Mendapat predikat : A", "| Dinyatakan Lulus Mata Kuliah", mata_kuliah)
+    print("Mendapat predikat : A")
+    print("Dinyatakan Lulus Mata Kuliah", mata_kuliah)
 elif (nilai >= 71) and (nilai  <= 80):
-    print("Mendapat predikat : B", "| Dinyatakan Lulus Mata Kuliah", mata_kuliah)
+    print("Mendapat predikat : B")
+    print("Dinyatakan Lulus Mata Kuliah", mata_kuliah)
 elif (nilai >= 61) and (nilai <= 70):
-    print("Mendapat predikat : C", "| Dinyatakan Lulus Mata Kuliah", mata_kuliah)
+    print("Mendapat predikat : C")
+    print("Dinyatakan Lulus Mata Kuliah", mata_kuliah)
 else:
     print("Harus Mengulang Mata Kuliah", mata_kuliah)
